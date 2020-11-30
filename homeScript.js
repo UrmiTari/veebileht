5ff131a5d5949fe15d8bf007a03db6f48a3f6360
Urmi ❤ <urmitari@gmail.comm>
2020-11-30 17:50:47 +0200

    button

---
+++ homeScript.js
@@ -0,0 +1,8 @@
+//muutuja bleep on uus audio element
+var laugh = new Audio();
+//lähtefail
+laugh.src = "laugh.mp3";
+//funktsioon heli mängimiseks
+function playBtnSound (num) {
+    laugh.play(); 
+}
