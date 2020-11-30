247608d0a8a600f422088a309b54942702a8e385
Urmi ❤ <urmitari@gmail.comm>
2020-11-30 11:30:56 +0200

    parandused

--- myScript.js
+++ myScript.js
@@ -1,15 +1,20 @@
+//Võtab html failist id põhjal nupu ja omistab selle muutujale mybutton
 var mybutton = document.getElementById("myBtn");
 
+//kontollime, kui kaguele kasutaja on kerinud.
+// kui kasutaja on alla poole kerinud 20px lehe ülemisest äärest hakkame talle näitama nuppu, 
+//enne seda näha ei ole.
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
+// Kui nupu peale vajutada, siis keritakse tagasi lehe algusesse.
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
